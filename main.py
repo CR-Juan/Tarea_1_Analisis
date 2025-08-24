ceda614d7dc002e0f6ca6c6319b7c491ff326964
1Paul2 <160780913+1Paul2@users.noreply.github.com>
2025-08-23 22:18:20 -0600

    segunda parte que elimine por error

--- main.py
+++ main.py
@@ -77,4 +77,70 @@ Memoria Actual: 10098968
 Memoria Pico: 10099164
 """
 
+"""""
+=========================================
+            Funciones de prueba
+=========================================
+"""
+def Numero_aleatorio():
+    return random.randint(100, 100000)
+def Crear_Matriz_2(filas, columnas):
+    matriz = []
+    for _ in range(filas):
+        fila = []
+        for _ in range(columnas):
+            fila += [Numero_aleatorio()]
+        matriz += [fila]
+    
+    return matriz
+
+"""
+
+=========================================
+           Funciones de Busqueda
+=========================================
+"""
+def BusquedaBinaria_matriz(matriz, objetivo):
+    for fila in matriz:
+        if BusquedaBinaria(fila, objetivo):
+            return True
+    return False
+
+def BusquedaBinaria(lista, objetivo):
+    izq = 0
+    derecha = len(lista) - 1
+    while izq <= derecha:
+        mitad = izq + (derecha - izq) // 2
+        if (lista[mitad] == objetivo):
+            return True
+        elif (lista[mitad] < objetivo):
+            izq = mitad + 1
+        else:
+            derecha = mitad - 1
+    return False
+
+def busqueda_matriz(matriz, objetivo):
+    for lista in matriz:
+        if busqueda_matriz_rec(lista, objetivo):
+            return True
+    return False
+
+def busqueda_matriz_rec(lista, objetivo):
+    if lista == []:
+        return False
+
+    mitad = len(lista) // 2
+    izq = lista[:mitad]
+    derecha = lista[mitad:]
+
+    while (izq != [] or derecha != []):
+        if izq != []:
+            if izq[0] == objetivo:
+                return True
+            izq = izq[1:]
+
+        if derecha != []:
+            if derecha[0] == objetivo:
+                return True
+            derecha = derecha[1:]
 
